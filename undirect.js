2b192b51cece45c9ce9ae75d2a65e252a10863df
Gary <paicha.me@gmail.com>
2016-11-23 18:24:37 +0800

    CHG: refactor

--- undirect.js
+++ undirect.js
@@ -1,14 +1,22 @@
 function scriptToExecute() {
     function replaceLinks() {
-        $('a[href*="link.zhihu.com"]').map(function() {
-            this.href = decodeURIComponent(this.href.split('target=')[1]);
-        });
+        var links = document.querySelectorAll('a[href*="link.zhihu.com/?target="]');
+        for (var i = 0; i < links.length; i++) {
+            var link = links[i];
+            link.href = decodeURIComponent(link.href.split('target=')[1]);
+        }
     }
     replaceLinks();
-    document.body.addEventListener("DOMNodeInserted", function() {
+
+    var observer = new MutationObserver(function(mutations) {
         replaceLinks();
     });
+    observer.observe(document, {
+        subtree: true,
+        attributes: true
+    });
 }
+
 var script = document.createElement('script');
 script.appendChild(document.createTextNode('(' + scriptToExecute + ')();'));
 (document.body || document.head || document.documentElement).appendChild(script);
